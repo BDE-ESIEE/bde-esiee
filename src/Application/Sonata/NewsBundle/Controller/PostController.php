d8a871387c40b2d86077127248ae566287660731
Claros <naji.demolitionman@gmail.com>
2014-07-10 21:22:01 +0200

    Add news pagination

---
+++ src/Application/Sonata/NewsBundle/Controller/PostController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Application\Sonata\NewsBundle\Controller;
+
+use Sonata\NewsBundle\Controller\PostController as BaseController;
+
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Sonata\NewsBundle\Model\CommentInterface;
+use Sonata\NewsBundle\Model\PostInterface;
+
+class PostController extends BaseController
+{
+    /**
+     * @param array $criteria
+     * @param array $parameters
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function renderArchive(array $criteria = array(), array $parameters = array())
+    {
+        $pager = $this->getPostManager()->getPager(
+            $criteria,
+            $this->getRequest()->get('page', 1)
+        );
+
+        $pager->setMaxPerPage(4);
+        $pager->setPage($this->getRequest()->get('page', 1));
+        $pager->getQuery()->setMaxResults(null);
+        $pager->getQuery()->setFirstResult(null);
+        $pager->getQuery()->setSortBy(array(), array('fieldName'=>'publicationDateStart'));
+        $pager->init();
+
+        $parameters = array_merge(array(
+            'pager'            => $pager,
+            'blog'             => $this->get('sonata.news.blog'),
+            'tag'              => false,
+            'collection'       => false,
+            'route'            => $this->getRequest()->get('_route'),
+            'route_parameters' => $this->getRequest()->get('_route_params')
+        ), $parameters);
+
+        $response = $this->render(sprintf('SonataNewsBundle:Post:archive.%s.twig', $this->getRequest()->getRequestFormat()), $parameters);
+
+        if ('rss' === $this->getRequest()->getRequestFormat()) {
+            $response->headers->set('Content-Type', 'application/rss+xml');
+        }
+
+        return $response;
+    }
+
+    public function sideMenuAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $tag_list = $em->getRepository('ApplicationSonataClassificationBundle:Tag')->findBy(array('enabled' => true));
+        $cat_list = $em->getRepository('ApplicationSonataClassificationBundle:Collection')->findBy(array('enabled' => true));
+
+        return $this->render('ApplicationSonataNewsBundle:Post:sideMenu.html.twig', array(
+            'tag_list' => $tag_list,
+            'cat_list' => $cat_list,
+        ));
+    }
+}
