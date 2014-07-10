fad548294a6d856654568a53a77f4fb8e4a3e556
Claros <naji.demolitionman@gmail.com>
2014-07-10 11:52:37 +0200

    Fix ICS formatter

--- src/Application/BDEBundle/Decoder/ICSDecoder.php
+++ src/Application/BDEBundle/Decoder/ICSDecoder.php
@@ -18,13 +18,13 @@ class ICSDecoder implements DecoderInterface
 
     public function decode($data)
     {
-        $ics = "BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//hacksw/handcal//NONSGML v1.0//EN\r\nCALSCALE:GREGORIAN\r\n";
+        $ics = "BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//BDE ESIEE Paris//FR\r\nX-WR-CALNAME:Evénements du BDE\r\nX-WR-TIMEZONE:Europe/Berlin\r\nCALSCALE:GREGORIAN\r\n";
 
 		foreach ($data as $event) {
 			$ics .= "BEGIN:VEVENT\r\nUID:".$event['id']."\r\nLOCATION:".$this->escapeString($event['place'])."\r\nSUMMARY:".$this->escapeString($event['title'])."\r\nDTSTART:".$this->dateToCal($event['start'])."\r\nDTEND:".$this->dateToCal($event['end'])."\r\nEND:VEVENT\r\n";
 		}
 
-		$ics .= "END:VCALENDAR\r\n";
+		$ics .= "END:VCALENDAR";
 
 		return $ics;
     }
