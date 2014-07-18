eaeea4ee5e3867b0b5c084db06ad81a9b14a0e72
Claros <naji.demolitionman@gmail.com>
2014-07-18 12:23:56 +0200

    Fix YouTube video in media

--- src/Application/Sonata/MediaBundle/Controller/GalleryController.php
+++ src/Application/Sonata/MediaBundle/Controller/GalleryController.php
@@ -21,4 +21,33 @@ class GalleryController extends BaseController
             'galleries'   => $galleries,
         ));
     }
+
+    /**
+     * @param string $id
+     *
+     * @return \Symfony\Bundle\FrameworkBundle\Controller\Response
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     */
+    public function viewAction($id)
+    {
+        $gallery = $this->get('sonata.media.manager.gallery')->findOneBy(array(
+            'id'      => $id,
+            'enabled' => true
+        ));
+
+        if (!$gallery) {
+            throw new NotFoundHttpException('Impossible de trouver une galerie avec cet id');
+        }
+
+        $youtube = false;
+        foreach ($gallery->getGalleryHasMedias() as $galleryHasMedia) {
+            if ($galleryHasMedia->getMedia()->getProviderName() == 'sonata.media.provider.youtube' || $galleryHasMedia->getMedia()->getProviderName() == 'sonata.media.provider.dailymotion')
+                $youtube = true;
+        }
+
+        return $this->render('SonataMediaBundle:Gallery:view.html.twig', array(
+            'gallery'   => $gallery,
+            'youtube'   => $youtube,
+        ));
+    }
 }
