1f9103bfce3dd6ea4df9ba54288b86fea3607d51
na-ji <naji.demolitionman@gmail.com>
2014-08-31 10:50:41 +0200

    Fix registration

---
+++ src/Application/Sonata/UserBundle/Controller/RegistrationController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Application\Sonata\UserBundle\Controller;
+
+use FOS\UserBundle\Controller\RegistrationController as BaseController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class RegistrationController extends BaseController
+{
+    public function registerBisAction()
+    {
+        $form = $this->container->get('fos_user.registration.form');
+        $formHandler = $this->container->get('fos_user.registration.form.handler');
+        $confirmationEnabled = $this->container->getParameter('fos_user.registration.confirmation.enabled');
+
+        $process = $formHandler->process($confirmationEnabled);
+        if ($process) {
+            $user = $form->getData();
+
+            $authUser = false;
+            if ($confirmationEnabled) {
+                $this->container->get('session')->set('fos_user_send_confirmation_email/email', $user->getEmail());
+                $route = 'fos_user_registration_check_email';
+            } else {
+                $authUser = true;
+                $route = 'fos_user_registration_confirmed';
+            }
+
+            $this->setFlash('fos_user_success', 'registration.flash.user_created');
+            $url = $this->container->get('router')->generate($route);
+            $response = new RedirectResponse($url);
+
+            if ($authUser) {
+                $this->authenticateUser($user, $response);
+            }
+
+            return $response;
+        }
+
+        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:register_content.html.'.$this->getEngine(), array(
+            'form' => $form->createView(),
+        ));
+    }
+}
