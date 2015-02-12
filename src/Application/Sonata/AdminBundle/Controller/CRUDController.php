858f44cb25a4e60643dc677311ee2dcfe0cbe244
na-ji <naji.demolitionman@gmail.com>
2015-02-12 22:27:02 +0100

    Fix Student ACL

--- src/Application/Sonata/AdminBundle/Controller/CRUDController.php
+++ src/Application/Sonata/AdminBundle/Controller/CRUDController.php
@@ -3,6 +3,8 @@
 namespace Application\Sonata\AdminBundle\Controller;
 
 use Sonata\AdminBundle\Controller\CRUDController as BaseController;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Sonata\AdminBundle\Exception\ModelManagerException;
 
 class CRUDController extends BaseController
 {
@@ -38,5 +40,101 @@ class CRUDController extends BaseController
 			'action' => 'trombi',
 			'object' => $object,
         ));
+	}
+
+	/**
+	 * Create action
+	 *
+	 * @return Response
+	 *
+	 * @throws AccessDeniedException If access is not granted
+	 */
+	public function createAction()
+	{
+		// the key used to lookup the template
+		$templateKey = 'edit';
+
+		if (false === $this->admin->isGranted('CREATE')) {
+			throw new AccessDeniedException();
+		}
+
+		$object = $this->admin->getNewInstance();
+
+		$this->admin->setSubject($object);
+
+		/** @var $form \Symfony\Component\Form\Form */
+		$form = $this->admin->getForm();
+		$form->setData($object);
+
+		if ($this->getRestMethod()== 'POST') {
+			$form->submit($this->get('request'));
+
+			$isFormValid = $form->isValid();
+
+			// persist if the form was valid and if in preview mode the preview was approved
+			if ($isFormValid && (!$this->isInPreviewMode() || $this->isPreviewApproved())) {
+
+				if (false === $this->admin->isGranted('CREATE')) {
+					throw new AccessDeniedException();
+				}
+
+				try {
+					$object = $this->admin->create($object);
+
+					if ($this->isXmlHttpRequest()) {
+						return $this->renderJson(array(
+							'result' => 'ok',
+							'objectId' => $this->admin->getNormalizedIdentifier($object)
+						));
+					}
+
+					$this->addFlash(
+						'sonata_flash_success',
+						$this->admin->trans(
+							'flash_create_success',
+							array('%name%' => $this->escapeHtml($this->admin->toString($object))),
+							'SonataAdminBundle'
+						)
+					);
+
+					// redirect to edit mode
+					return $this->redirectTo($object);
+
+				} catch (ModelManagerException $e) {
+					$this->handleModelManagerException($e);
+
+					$isFormValid = false;
+				}
+			}
+
+			// show an error message if the form failed validation
+			if (!$isFormValid) {
+				if (!$this->isXmlHttpRequest()) {
+					$this->addFlash(
+						'sonata_flash_error',
+						$this->admin->trans(
+							'flash_create_error',
+							array('%name%' => $this->escapeHtml($this->admin->toString($object))),
+							'SonataAdminBundle'
+						)
+					);
+				}
+			} elseif ($this->isPreviewRequested()) {
+				// pick the preview template if the form was valid and preview was requested
+				$templateKey = 'preview';
+				$this->admin->getShow();
+			}
+		}
+
+		$view = $form->createView();
+
+		// set the theme for the current Admin Form
+		$this->get('twig')->getExtension('form')->renderer->setTheme($view, $this->admin->getFormTheme());
+
+		return $this->render($this->admin->getTemplate($templateKey), array(
+			'action' => 'create',
+			'form'   => $view,
+			'object' => $object,
+		));
 	}
 }
